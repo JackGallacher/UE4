c50a954442e8464d11e37624a90df989072e5d0a
Jack Gallacher <jackleslie.gallacher@gmail.com>
2016-03-17 17:35:54 +0000

    Updated .gitignore
    Updated .gitignore

--- Learning/Intermediate/Build/Win64/UE4Editor/Development/Learning/PCH.Learning.h.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:\Users\Jack\Documents\GitHub\UE4\Learning\Source\Learning\Learning.h"
--- Learning/Source/Learning/LearningGameMode.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Learning.h"
-#include "LearningGameMode.h"
-
-
-
-
-
